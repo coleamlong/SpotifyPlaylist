b731fbbd1754f06f05a807e9bf2f15dcbcc1c339
Cole Amlong <coleamlong@gmail.com>
2024-06-16 04:04:41 -0500

    changed app redirect uri for spotify web api

--- src/constants/Spotify.jsx
+++ src/constants/Spotify.jsx
@@ -1,8 +1,7 @@
 import SpotifyWebApi from "spotify-web-api-js";
 
 const CLIENT_ID = "91d037f7894441b2aab67c0acec6689d";
-// const REDIRECT_URI = "https://spotify-playlist.coleamlong.com";
-const REDIRECT_URI = "http://localhost:3000";
+const REDIRECT_URI = "https://spotify-playlist.coleamlong.com";
 const AUTH_URL = "https://accounts.spotify.com/authorize";
 const SCOPES = [
     "user-top-read",
